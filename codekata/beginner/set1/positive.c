6e6ab22b5649c4669c5f2afe86a2de58221180ba
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2017-12-19 09:18:43 +0000

    Create positive.c

---
+++ codekata/beginner/set1/positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+int num;
+printf("Enter a number:");
+scanf("%d",&num);
+if(num>0)
+{
+printf("Positive");
+}
+else if(num<0)
+{
+printf("Negative");
+}
+else
+printf("Zero");
+}
