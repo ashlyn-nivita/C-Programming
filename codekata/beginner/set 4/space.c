7c4a00b03db06a7502ec7155f475c94e7c0c42e6
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2018-05-16 19:39:14 +0530

    Create space.c

---
+++ codekata/beginner/set 4/space.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void) 
+{
+	char str[100];
+	int i,count=0,length,space=0;
+	gets(str);
+	length=strlen(str);
+	for(i=0;i<length;i++)
+	{
+		if((str[i]>='A'&& str[i]<='Z')||(str[i]>='a'&& str[i]<='z'))
+		{
+			count++;
+		}
+		else
+		{
+			space++;
+		}
+	}
+	printf("%d",space);
+}
